ed5036d3afd2c59e01207cac6eb57624ee114c39
kikuke <kkkjs0814@naver.com>
2023-05-26 12:37:43 +0900

    [Init]

---
+++ src/main/java/kikuke/appleauth/AppleauthApplication.java
@@ -0,0 +1,13 @@
+package kikuke.appleauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kikuke/appleauth/AppleauthApplicationTests.java
@@ -0,0 +1,13 @@
+package kikuke.appleauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
